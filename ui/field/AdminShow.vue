a8836f64a820f8c4904ac95e6cf5ab181e15e61e
Charlie Page <charliepage88@gmail.com>
2020-07-04 17:37:09 -0400

    package fields/cleanup work/refactoring

--- ui/field/AdminShow.vue
+++ ui/field/AdminShow.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default {
     'value',
     'field',
     'model',
-    'module',
+    'package',
     'action'
   ]
 }
